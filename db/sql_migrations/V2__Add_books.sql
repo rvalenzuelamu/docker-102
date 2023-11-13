5eb2530a9740207638fea515b27119a6fc3c22ff
Robert Alexis Valenzuela Murillo <RobertValenzuela@vidacamara.cl>
2023-11-13 17:20:31 -0300

    Handson realizado

---
+++ db/sql_migrations/V2__Add_books.sql
@@ -0,0 +1,2 @@
+INSERT INTO books (title, author, pages_num, review) VALUES('A Tale of Two Cities', 'Charles Dickens', 489, 'A great classic!');
+INSERT INTO books (title, author, pages_num, review) VALUES('Anna Karenina', 'Leo Tolstoy', 864, 'Another great classic!');
